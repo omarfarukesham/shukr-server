a334de03b5efef682c24601bb819a048c2422bc0
omarfarukesham <omar.lu86@gmail.com>
2025-03-04 11:16:08 +0600

    feat: challenge authentication has removed

--- src/module/challenges/challenge.router.ts
+++ src/module/challenges/challenge.router.ts
@@ -7,10 +7,16 @@ import { USER_ROLE } from "../user/user.constants";
 const ChallengeRouter = Router();
 
 
-ChallengeRouter.post("/", auth(USER_ROLE.admin, USER_ROLE.user), challengeController.createChallenge);
-ChallengeRouter.get("/", auth(USER_ROLE.admin, USER_ROLE.user), challengeController.getChallenges);
-ChallengeRouter.get("/:id",  auth(USER_ROLE.admin, USER_ROLE.user), challengeController.getSingleChallenge);
-ChallengeRouter.patch("/:id", auth(USER_ROLE.admin, USER_ROLE.user), challengeController.updateChallenge);
-ChallengeRouter.delete("/:id", auth(USER_ROLE.admin, USER_ROLE.user), challengeController.deleteChallenge);
+// ChallengeRouter.post("/", auth(USER_ROLE.admin, USER_ROLE.user), challengeController.createChallenge);
+// ChallengeRouter.get("/", auth(USER_ROLE.admin, USER_ROLE.user), challengeController.getChallenges);
+// ChallengeRouter.get("/:id",  auth(USER_ROLE.admin, USER_ROLE.user), challengeController.getSingleChallenge);
+// ChallengeRouter.patch("/:id", auth(USER_ROLE.admin, USER_ROLE.user), challengeController.updateChallenge);
+// ChallengeRouter.delete("/:id", auth(USER_ROLE.admin, USER_ROLE.user), challengeController.deleteChallenge);
+
+ChallengeRouter.post("/",  challengeController.createChallenge);
+ChallengeRouter.get("/",   challengeController.getChallenges);
+ChallengeRouter.get("/:id",   challengeController.getSingleChallenge);
+ChallengeRouter.patch("/:id",  challengeController.updateChallenge);
+ChallengeRouter.delete("/:id",  challengeController.deleteChallenge);
 
 export default ChallengeRouter;
