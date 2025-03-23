0bd82cbab4e28618c69cdbf86b0271908a9348f6
Sakib Rahman <seu.sakib@gmail.com>
2025-03-23 16:25:00 +0600

    fix ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,8 +8,9 @@ module.exports = {
       autorestart: true,
       watch: false,
       max_memory_restart: "1G",
+      env_file: ".env",
       env: {
-        PORT: process.env.PORT || 5000,
+        PORT: process.env.PORT,
         DATABASE_URL: process.env.DATABASE_URL,
         BCRYPT_SALT_ROUNDS: process.env.BCRYPT_SALT_ROUNDS,
         JWT_SECRET: process.env.JWT_SECRET,
